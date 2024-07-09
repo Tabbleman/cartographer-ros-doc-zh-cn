8be615b088aa9c6ebda5614b683d06e2516c42cb
Tabbleman <tabbleman11@gmail.com>
2024-07-09 14:23:38 +0800

    update conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -35,30 +35,21 @@ latex_elements = {
 # -- Options for manual page output ------------------------------------------
 
 man_pages = [
-    (master_doc, 'yourprojectname', 'Your Project Documentation',
+    (master_doc, 'cartographer-ros-doc-zh-cn', 'Cartographer ROS Documentation (Chinese)',
      [author], 1)
 ]
 
 # -- Options for Texinfo output ----------------------------------------------
 
 texinfo_documents = [
-    (master_doc, 'YourProjectName', 'Your Project Documentation',
-     author, 'YourProjectName', 'One line description of project.',
+    (master_doc, 'cartographer-ros-doc-zh-cn', 'Cartographer ROS Documentation (Chinese)',
+     author, 'cartographer-ros-doc-zh-cn', 'Documentation for Cartographer ROS (Chinese)',
      'Miscellaneous'),
 ]
 
 # -- Extension configuration -------------------------------------------------
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-htmlhelp_basename = 'YourProjectNamedoc'
-
-# -- Other configuration ----------------------------------------------------
-
-# At the bottom of conf.py
+# Add custom CSS files to the static path and then define them in html_static_path
 def setup(app):
-    app.add_stylesheet('custom.css')
+    app.add_css_file('custom.css')
 
