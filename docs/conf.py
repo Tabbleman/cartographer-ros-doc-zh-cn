8cf54e11a6d1c8ac13cff2a6535455f3a5ca5ccb
Tabbleman <tabbleman11@gmail.com>
2024-07-09 14:48:04 +0800

    add requirement and update config.

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Options for LaTeX output ------------------------------------------------
